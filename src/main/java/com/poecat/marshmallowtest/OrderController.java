6f2b7b12d38345a54aee6fac4c3b6ef4fb4c59df
Kasia <katarzyna.tramea@gmail.com>
2021-11-13 18:30:51 +0100

    added processOrder to OrderController.java

--- src/main/java/com/poecat/marshmallowtest/OrderController.java
+++ src/main/java/com/poecat/marshmallowtest/OrderController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -15,6 +16,12 @@ public class OrderController {
     public String orderForm(Model model) {
         model.addAttribute("order", new Order());
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order) {
+        log.info("Order has been made successfully: " + order);
+        return "redirect:/";
     }
 
 
