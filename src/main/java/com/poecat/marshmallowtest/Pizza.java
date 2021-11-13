5d6a0c03205899bca7f2fd622c8fd877d233465a
Kasia <katarzyna.tramea@gmail.com>
2021-11-13 18:44:02 +0100

    update Pizza.java with validation annotations

--- src/main/java/com/poecat/marshmallowtest/Pizza.java
+++ src/main/java/com/poecat/marshmallowtest/Pizza.java
@@ -1,11 +1,17 @@
 package com.poecat.marshmallowtest;
 
 import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Pizza {
 
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
+    @Size(min = 1, message = "You have to choose at least one ingredient")
     private List<String> ingredients;
 }
