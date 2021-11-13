4ed941197b763752555e815bb29e0bc6f79abacf
Kasia <katarzyna.tramea@gmail.com>
2021-11-13 17:12:52 +0100

    added orderForm() to OrderController.java

--- src/main/java/com/poecat/marshmallowtest/OrderController.java
+++ src/main/java/com/poecat/marshmallowtest/OrderController.java
@@ -2,6 +2,8 @@ package com.poecat.marshmallowtest;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -9,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/orders")
 public class OrderController {
 
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
 
 
 
