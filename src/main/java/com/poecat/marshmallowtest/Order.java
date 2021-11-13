bc5877559eb572b8bc6ee55f01cc92aa3f2c8aad
Kasia <katarzyna.tramea@gmail.com>
2021-11-13 18:17:24 +0100

    updated Order.java with validation annotations & addDesign()

---
+++ src/main/java/com/poecat/marshmallowtest/Order.java
@@ -0,0 +1,51 @@
+package com.poecat.marshmallowtest;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.hibernate.validator.constraints.NotBlank;
+
+import lombok.Data;
+
+@Data
+public class Order {
+
+    private Long id;
+    private Date placedAt;
+
+    @NotBlank(message = "The name field cannot be empty.")
+    private String deliveryName;
+
+    @NotBlank(message = "The street field cannot be empty.")
+    private String deliveryStreet;
+
+    @NotBlank(message = "The city field cannot be empty.")
+    private String deliveryCity;
+
+    @NotBlank(message = "The state field cannot be empty.")
+    private String deliveryState;
+
+    @NotBlank(message = "The zip code field cannot be empty.")
+    private String deliveryZip;
+
+    @CreditCardNumber(message = "Invalid card number.")
+    private String ccNumber;
+
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+            message = "Correct date form: MM/YY")
+    private String ccExpiration;
+
+    @Digits(integer = 3, fraction = 0, message = "Invalid CVV code")
+    private String ccCVV;
+
+    private List<Pizza> pizza = new ArrayList<>();
+
+    public void addDesign(Pizza design) {
+        this.pizza.add(design);
+    }
+
+}
