1a0b7c7f2432e6ee12df013da97a11db19d87473
Kasia <katarzyna.tramea@gmail.com>
2021-11-13 17:10:46 +0100

    created OrderController.java

---
+++ src/main/java/com/poecat/marshmallowtest/OrderController.java
@@ -0,0 +1,16 @@
+package com.poecat.marshmallowtest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+
+
+
+
+}
