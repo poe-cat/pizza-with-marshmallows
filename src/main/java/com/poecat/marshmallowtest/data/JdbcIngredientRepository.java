c80052553a46cb3129a81d1864421093c5a5815b
Kasia <katarzyna.tramea@gmail.com>
2021-11-14 13:50:52 +0100

    added constructor in JdbcIngredientRepository

--- src/main/java/com/poecat/marshmallowtest/data/JdbcIngredientRepository.java
+++ src/main/java/com/poecat/marshmallowtest/data/JdbcIngredientRepository.java
@@ -1,10 +1,19 @@
 package com.poecat.marshmallowtest.data;
 
 import com.poecat.marshmallowtest.Ingredient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class JdbcIngredientRepository implements IngredientRepository {
+
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    public JdbcIngredientRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
 
     @Override
     public Iterable<Ingredient> findAll() {
