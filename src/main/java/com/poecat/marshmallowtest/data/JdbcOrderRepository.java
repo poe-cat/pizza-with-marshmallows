2436d7ac699cc784ce04ec7e55ab37ea273f8141
Kasia <katarzyna.tramea@gmail.com>
2021-11-17 18:24:13 +0100

    added constructor to JdbcOrderRepository

--- src/main/java/com/poecat/marshmallowtest/data/JdbcOrderRepository.java
+++ src/main/java/com/poecat/marshmallowtest/data/JdbcOrderRepository.java
@@ -2,13 +2,32 @@ package com.poecat.marshmallowtest.data;
 
 import com.poecat.marshmallowtest.Order;
 import com.poecat.marshmallowtest.Pizza;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Repository
 public class JdbcOrderRepository implements OrderRepository{
+
+    private SimpleJdbcInsert orderInserter;
+    private SimpleJdbcInsert orderPizzaInserter;
+    private ObjectMapper objectMapper;
+
+    public JdbcOrderRepository(JdbcTemplate jdbc) {
+        this.orderInserter = new SimpleJdbcInsert(jdbc)
+                .withTableName("Pizza_Order")
+                .usingGeneratedKeyColumns("id");
+
+        this.orderPizzaInserter = new SimpleJdbcInsert(jdbc)
+                .withTableName("Pizza_Order_Pizzas");
+
+        this.objectMapper = new ObjectMapper();
+    }
 
     @Override
     public Order save(Order order) {
