94bf8efa939373d6ca899dd9ec6244a625a830d2
Kasia <katarzyna.tramea@gmail.com>
2021-11-17 17:19:29 +0100

    created IngredientByIdConverter.java

--- src/main/java/com/poecat/marshmallowtest/web/IngredientByIdConverter.java
+++ src/main/java/com/poecat/marshmallowtest/web/IngredientByIdConverter.java
@@ -1,7 +1,23 @@
 package com.poecat.marshmallowtest.web;
 
+import com.poecat.marshmallowtest.Ingredient;
+import com.poecat.marshmallowtest.data.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class IngredientByIdConverter {
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+
+    private IngredientRepository ingredientRepo;
+
+    @Autowired
+    public IngredientByIdConverter(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+
+    @Override
+    public Ingredient convert(String id) {
+        return ingredientRepo.findById(id);
+    }
 }
