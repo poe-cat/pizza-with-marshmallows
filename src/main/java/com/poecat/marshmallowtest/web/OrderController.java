1fbcaf6a89778c5527097f4cdd9db9bd772e00bd
Kasia <katarzyna.tramea@gmail.com>
2021-11-17 19:35:20 +0100

    updated OrderController.java with constructor and implemented repositories

--- src/main/java/com/poecat/marshmallowtest/web/OrderController.java
+++ src/main/java/com/poecat/marshmallowtest/web/OrderController.java
@@ -1,31 +1,44 @@
 package com.poecat.marshmallowtest.web;
 
 import com.poecat.marshmallowtest.Order;
-import lombok.extern.slf4j.Slf4j;
+import com.poecat.marshmallowtest.data.OrderRepository;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
-@Slf4j
+import javax.validation.Valid;
+
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
 
+    private OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     @GetMapping("/current")
-    public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
+    public String orderForm() {
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(Order order) {
-        log.info("Order has been made successfully: " + order);
+    public String processOrder(@Valid Order order, Errors errors,
+                               SessionStatus sessionStatus) {
+
+        if(errors.hasErrors()) {
+          return "orderForm";
+        }
+
+        orderRepository.save(order);
+        sessionStatus.setComplete();
         return "redirect:/";
     }
-
-
-
-
 }
